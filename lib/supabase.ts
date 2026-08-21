958bb9202578bd4632d82da7fa3adfb096ff4c9d
otech2208-collab <otech2208@gmail.com>
2026-08-21 04:21:28 -0400

    Update supabaseUrl to a hardcoded Supabase URL
    Replaced supabaseUrl with a hardcoded URL.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.https:tpmcwjpkevihoofsqaux.supabase.co/rest/v1;
 const serviceRoleKey = process.env.sb_secret_0f0UW_MxhZVMvfSpGVrmqQ_Rqd3C-87;
 export const supabase = supabaseUrl && serviceRoleKey ? createClient(supabaseUrl, serviceRoleKey, { auth: { autoRefreshToken: false, persistSession: false } }) : null;
